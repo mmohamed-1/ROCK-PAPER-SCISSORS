e79075e8c6308272999d0598a6b1d585f7534b64
mohamed <m.hagle2@gmail.com>
2022-03-05 11:19:04 +0000

    updated the app.js and style.css

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
-// let answer = prompt("choose rock, paper or scissors");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", function () {
+  console.log(playRound("rock", computerPlay()));
+});
+paper.addEventListener("click", function () {
+  console.log(playRound("Paper", computerPlay()));
+});
+scissors.addEventListener("click", function () {
+  console.log(playRound("scissors", computerPlay()));
+});
 
 function computerPlay() {
-  let choice = ["rock", "paper", "scissors"];
+  const choice = ["rock", "paper", "scissors"];
   let picked = Math.floor(Math.random() * choice.length);
   let result = choice[picked];
   return result;
 }
 
 function playRound(playerSelection, computerSelection) {
-  answer = playerSelection.toLowerCase();
-
-  if (answer === "rock" && computerSelection === "scissors") {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
     return "Player Wins! rock beats scissors";
   }
-  if (answer === "paper" && computerSelection === "rock") {
+  if (playerSelection === "paper" && computerSelection === "rock") {
     return "Player Wins! paper beats rock";
   }
-  if (answer === "scissors" && computerSelection === "paper") {
+  if (playerSelection === "scissors" && computerSelection === "paper") {
     return "Player Wins! scissors beats paper";
-  } else if (computerSelection === "rock" && answer === "scissors") {
-    return "You Lose! Rock beats Paper";
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    return "You Lose! Rock beats scissors";
   }
-  if (computerSelection === "paper" && answer === "rock") {
+  if (computerSelection === "paper" && playerSelection === "rock") {
     return "You Lose! Paper beats Rock";
   }
-  if (computerSelection === "scissors" && answer === "paper") {
+  if (computerSelection === "scissors" && playerSelection === "paper") {
     return "You Lose! scissors beats paper";
   } else {
     return "draw";
   }
 }
 
-// console.log(playRound(playerSelection, computerPlay()));
+// console.log
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(answer, computerPlay()));
-  }
-}
-
-console.log(game());
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(playRound("rock", computerPlay()));
+//   }
+// }
